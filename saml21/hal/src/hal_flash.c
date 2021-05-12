a180fdc17f4a49eeaa39b17bb950908693cf227d
sabas1080 <sabasjimenez@gmail.com>
2021-05-12 02:14:39 -0500

    fix const flash append

--- saml21/hal/src/hal_flash.c
+++ saml21/hal/src/hal_flash.c
@@ -125,7 +125,7 @@ int32_t flash_write(struct flash_descriptor *flash, uint32_t dst_addr, uint8_t *
 /**
  * \brief Appends a number of bytes to a page in the internal Flash
  */
-int32_t flash_append(struct flash_descriptor *flash, uint32_t dst_addr, uint8_t *buffer, uint32_t length)
+int32_t flash_append(struct flash_descriptor *flash, uint32_t dst_addr, const uint8_t *buffer, uint32_t length)
 {
 	ASSERT(flash && buffer && length);
 
